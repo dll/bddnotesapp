cb57de69b94309e9692f26946749ab8e88a80789
czldl <ldl@chzu.edu.cn>
2016-11-16 08:30:33 +0800

    part2

--- spec/AppSpec.js
+++ spec/AppSpec.js
@@ -1,22 +1,37 @@
 ﻿
-describe("Notes public interface exists", function () {
+describe("Data context tests", function () {
 
-    it("Defines the app", function () {
-        expect(Notes.app).toBeDefined();
+    it("Exists in the app", function () {
+        expect(Notes.dataContext).toBeDefined();
     });
 
-    it("Has public function: getNotesList", function () {
-        expect(Notes.app.getNotesList).toBeDefined();
-    });
+    it("Returns notes Array", function () {
 
-});
-
-describe("Notes functions", function () {
-
-    it("Returns a list of notes", function () {
-
-        var notesList = Notes.app.getNotesList();
+        var notesList = Notes.dataContext.getNotesList();
 
         expect(notesList instanceof Array).toBeTruthy();
     });
+
+    it("Returns a blank note", function () {
+
+        var blankNote = Notes.dataContext.createBlankNote();
+        expect(blankNote.title.length === 0).toBeTruthy();
+        expect(blankNote.narrative.length === 0).toBeTruthy();
+    });
+
+    it("Has init function", function () {
+        expect(Notes.dataContext.init).toBeDefined();
+    });
+
+    it("Returns dummy notes saved in local storage", function () {
+
+        Notes.testHelper.createDummyNotes();
+        // Load dummy notes from local storage.
+        Notes.dataContext.init();
+
+        var notesList = Notes.dataContext.getNotesList();
+
+        expect(notesList.length > 0).toBeTruthy();
+    });      
+
 });
